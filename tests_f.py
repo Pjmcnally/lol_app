0642607cb9c43c415490874a81fc404133bb40b4
Pjmcnally <Patrick.J.McNally@gmail.com>
2015-12-01 14:30:14 -0800

    Functional Test now checks for input box

--- tests_f.py
+++ tests_f.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -18,44 +19,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # Bob notices it says "Patrick's LoL App" in the title.
         self.assertIn("Patrick's LoL App", self.browser.title)
-        self.fail("Finish writing tests")
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn("Patricks LoL App", header_text)
 
         # He is invited to enter a summoner name
+        inputbox = self.browser.find_element_by_id('summoner_name')
+        self.assertEqal(
+            inputbox.get_attribute('placeholder'),
+            'Enter a summoner name'
+        )
 
         # He types his summoner name into the field. 
+        inputbox.send_keys('Pjmcnally')
 
         # When he hits enter the page displays his unique summoner ID (first step).
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == 'Pjmcnally' for row in rows)
+        )
 
         # Bob is sutibly impressed (which is to say not very impressed).
 
+
+        self.fail("Finish writing tests")
+
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
