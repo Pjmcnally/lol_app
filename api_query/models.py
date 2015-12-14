9ac56c3b2307b69c21fa9f67a8ad6d3841a5ceb9
Pjmcnally <Patrick.J.McNally@gmail.com>
2015-12-14 13:15:55 -0800

    Add champion model to models

--- api_query/models.py
+++ api_query/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Champion(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=100, default='')
+    name = models.CharField(max_length=30, default='')
+    key = models.CharField(max_length=30, default='')
+
+    
