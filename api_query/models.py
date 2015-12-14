dab6d3eec3771061cc86cf3ae1c39cd11c136495
Pjmcnally <Patrick.J.McNally@gmail.com>
2015-12-14 15:24:48 -0800

    Add new models and logic to generate_fixtures.py

--- api_query/models.py
+++ api_query/models.py
@@ -3,8 +3,30 @@ from django.db import models
 # Create your models here.
 class Champion(models.Model):
     id = models.IntegerField(primary_key=True)
-    title = models.CharField(max_length=100, default='')
     name = models.CharField(max_length=30, default='')
-    key = models.CharField(max_length=30, default='')
+    descript = models.CharField(max_length=100, default='')
+    image = models.CharField(max_length=30, default='')
+    version = models.CharField(max_length=15, default='')
 
-    
+class Mastery(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=30, default='')
+    descript = models.CharField(max_length=100, default='')
+    tree = models.CharField(max_length=30, default='')
+    ranks = models.CharField(max_length=30, default='')
+    image = models.CharField(max_length=30, default='')
+    version = models.CharField(max_length=30, default='')
+
+class Rune(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=30, default='')
+    descript = models.CharField(max_length=100, default='')
+    image = models.CharField(max_length=30, default='')
+    version = models.CharField(max_length=30, default='')
+
+class Spell(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=30, default='')
+    descript = models.CharField(max_length=100, default='')
+    image = models.CharField(max_length=30, default='')
+    version = models.CharField(max_length=30, default='')
