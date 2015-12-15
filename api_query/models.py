4512dc3e4191ffeb6e7e3d2e64f5c54174659366
Pjmcnally <Patrick.J.McNally@gmail.com>
2015-12-15 09:56:20 -0800

    Change max length and field names in models

--- api_query/models.py
+++ api_query/models.py
@@ -3,30 +3,30 @@ from django.db import models
 # Create your models here.
 class Champion(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=30, default='')
-    descript = models.CharField(max_length=100, default='')
-    image = models.CharField(max_length=30, default='')
-    version = models.CharField(max_length=15, default='')
+    name = models.CharField(max_length=500, default='')
+    descript = models.CharField(max_length=500, default='')
+    image = models.CharField(max_length=500, default='')
+    version = models.CharField(max_length=500, default='')
 
 class Mastery(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=30, default='')
-    descript = models.CharField(max_length=100, default='')
-    tree = models.CharField(max_length=30, default='')
-    ranks = models.CharField(max_length=30, default='')
-    image = models.CharField(max_length=30, default='')
-    version = models.CharField(max_length=30, default='')
+    name = models.CharField(max_length=500, default='')
+    # descript = models.CharField(max_length=500, default='')
+    tree = models.CharField(max_length=500, default='')
+    ranks = models.CharField(max_length=500, default='')
+    image = models.CharField(max_length=500, default='')
+    version = models.CharField(max_length=500, default='')
 
 class Rune(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=30, default='')
-    descript = models.CharField(max_length=100, default='')
-    image = models.CharField(max_length=30, default='')
-    version = models.CharField(max_length=30, default='')
+    name = models.CharField(max_length=500, default='')
+    descript = models.CharField(max_length=500, default='')
+    image = models.CharField(max_length=500, default='')
+    version = models.CharField(max_length=500, default='')
 
 class Spell(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=30, default='')
-    descript = models.CharField(max_length=100, default='')
-    image = models.CharField(max_length=30, default='')
-    version = models.CharField(max_length=30, default='')
+    name = models.CharField(max_length=500, default='')
+    descript = models.CharField(max_length=500, default='')
+    image = models.CharField(max_length=500, default='')
+    version = models.CharField(max_length=500, default='')
