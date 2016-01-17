2b16abec4562d281cfaad9399e49d4dbb4bd5537
Pjmcnally <Patrick.J.McNally@gmail.com>
2016-01-17 14:39:39 -0800

    refactor JS and Jquery to reduce event listeners to 1 from 10

--- api_query/static/js/main.js
+++ api_query/static/js/main.js
@@ -1,54 +1,10 @@
 jQuery(document).ready(function($){
 	//open the lateral panel
-	$('.cd-btnb1').on('click', function(event){
+	$('.cd-btn').on('click', function(event){
 		event.preventDefault();
-		$('.cd-panelb1').addClass('is-visible');
-	});
-	//open the lateral panel
-	$('.cd-btnb2').on('click', function(event){
-		event.preventDefault();
-		$('.cd-panelb2').addClass('is-visible');
-	});
-		//open the lateral panel
-	$('.cd-btnb3').on('click', function(event){
-		event.preventDefault();
-		$('.cd-panelb3').addClass('is-visible');
-	});
-		//open the lateral panel
-	$('.cd-btnb4').on('click', function(event){
-		event.preventDefault();
-		$('.cd-panelb4').addClass('is-visible');
-	});
-		//open the lateral panel
-	$('.cd-btnb5').on('click', function(event){
-		event.preventDefault();
-		$('.cd-panelb5').addClass('is-visible');
-	});
-		//open the lateral panel
-	$('.cd-btnr1').on('click', function(event){
-		event.preventDefault();
-		$('.cd-panelr1').addClass('is-visible');
-	});
-		//open the lateral panel
-	$('.cd-btnr2').on('click', function(event){
-		event.preventDefault();
-		$('.cd-panelr2').addClass('is-visible');
-	});
-		//open the lateral panel
-	$('.cd-btnr3').on('click', function(event){
-		event.preventDefault();
-		$('.cd-panelr3').addClass('is-visible');
-	});
-		//open the lateral panel
-	$('.cd-btnr4').on('click', function(event){
-		event.preventDefault();
-		$('.cd-panelr4').addClass('is-visible');
-	});
-		//open the lateral panel
-	$('.cd-btnr5').on('click', function(event){
-		event.preventDefault();
-		$('.cd-panelr5').addClass('is-visible');
-	});
+		var parId = $(event.target).parent().attr("id");
+		$("."+parId).addClass('is-visible');
+	});	
 	
 	//clode the lateral panel
 	$('.cd-panel').on('click', function(event){
